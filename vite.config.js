0c590caba1901d4eb4645768ebf268fe543c2448
hantutido <nohtx@yahoo.com>
2025-02-28 16:29:36 +0800

    Rebuild project for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/scam-alert-web/",
+  base: "/scam-alert-web/", // Sesuai dengan nama repo GitHub Pages
   plugins: [react()],
 });
