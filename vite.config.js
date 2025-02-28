9b335ef6f0423162b22a83940cfec4ee44971b3a
hantutido <nohtx@yahoo.com>
2025-02-28 16:14:47 +0800

    Fix Vite base URL

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/scam-alert-web/",
   plugins: [react()],
-  base: '/scam-alert-web/', // Tambahkan base URL ini
 });
